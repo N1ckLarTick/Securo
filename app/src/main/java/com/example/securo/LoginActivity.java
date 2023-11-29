28e76ae1cffbdcb39ec2102c1b723e8f40c3eebf
b0r3nds <3borends3@gmail.com>
2023-11-29 21:32:00 +0700

    Global Update 3.2.12

--- app/src/main/java/com/example/securo/LoginActivity.java
+++ app/src/main/java/com/example/securo/LoginActivity.java
@@ -12,43 +12,49 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 private ActivityLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        binding = ActivityLoginBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null){
+            FirebaseAuth mAuth = FirebaseAuth.getInstance();
+            binding = ActivityLoginBinding.inflate(getLayoutInflater());
+            setContentView(binding.getRoot());
 
-        binding.LoginBtn.setOnClickListener(view ->{
-            String login = binding.EditLogin.getText().toString();
-            String password = binding.EditPassword.getText().toString();
-            if (login.isEmpty() || password.isEmpty()) {
-                Toast.makeText(this, "Введите данные", Toast.LENGTH_SHORT).show();
-            }
+            binding.LoginBtn.setOnClickListener(view ->{
+                String login = binding.EditLogin.getText().toString();
+                String password = binding.EditPassword.getText().toString();
+                if (login.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(this, "Введите данные", Toast.LENGTH_SHORT).show();
+                }
 
-            mAuth.signInWithEmailAndPassword(login, password)
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                Toast.makeText(LoginActivity.this, "Login succesful.", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                                startActivity(intent);
-                                finish();
-                            } else {
-                                Toast.makeText(LoginActivity.this, "Login failed.", Toast.LENGTH_SHORT).show();
+                mAuth.signInWithEmailAndPassword(login, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(LoginActivity.this, "Login succesful.", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "Login failed.", Toast.LENGTH_SHORT).show();
+                                }
                             }
-                        }
-                    });
+                        });
 
-        });
-        binding.RegisterAdviceTxt.setOnClickListener(view ->{
+            });
+            binding.RegisterAdviceTxt.setOnClickListener(view ->{
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
-        });
+            });} else {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        }
+
     }
-}
