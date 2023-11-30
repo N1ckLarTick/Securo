f5d90cf4ec3aefb14507b66fb67a8a601a7acf5c
b0r3nds <3borends3@gmail.com>
2023-11-30 23:09:23 +0700

    Update 6.3.16

--- app/src/main/java/com/example/securo/ui/home/QrFragment.java
+++ app/src/main/java/com/example/securo/ui/home/QrFragment.java
@@ -1,21 +1,17 @@
 package com.example.securo.ui.home;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-
+import android.widget.Toast;
+import android.content.SharedPreferences;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-
-import com.example.securo.R;
-import com.example.securo.databinding.FragmentProfileBinding;
 import com.example.securo.databinding.FragmentQrBinding;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
@@ -27,9 +23,9 @@ public class QrFragment extends Fragment {
 
     private FragmentQrBinding binding;
     public static String quer;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         QrViewModel qrViewModel =
                 new ViewModelProvider(this).get(QrViewModel.class);
 
@@ -39,17 +35,25 @@ public class QrFragment extends Fragment {
 
         binding.button.setOnClickListener(v1 -> {
             MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+            if (quer == null) {
+                SharedPreferences sharedPreferences = getActivity().getSharedPreferences("Query", Context.MODE_PRIVATE);
+                quer = sharedPreferences.getString("Q", quer);
+                if (quer == null) {
+                    Toast.makeText(getActivity().getApplicationContext(), "Вы не добавили пропуск", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                try {
+                    BitMatrix bitMatrix = multiFormatWriter.encode("https://firebasestorage.googleapis.com/v0/b/s3cur0.appspot.com/o/images%2F" + quer + "?alt=media", BarcodeFormat.QR_CODE, 300,300);
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
 
-            try {
-                BitMatrix bitMatrix = multiFormatWriter.encode("https://firebasestorage.googleapis.com/v0/b/s3cur0.appspot.com/o/images%2F" + quer + "?alt=media", BarcodeFormat.QR_CODE, 300,300);
-                BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
-                Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                    binding.qrCode.setImageBitmap(bitmap);
 
-                binding.qrCode.setImageBitmap(bitmap);
-
-            }catch (WriterException e){
-                throw new RuntimeException(e);
+                }catch (WriterException e){
+                    throw new RuntimeException(e);
+                }
             }
+
         });
 
         return root;
@@ -61,5 +65,7 @@ public class QrFragment extends Fragment {
         binding = null;
     }
 
+    void getQuery(){
+    }
 
 }
